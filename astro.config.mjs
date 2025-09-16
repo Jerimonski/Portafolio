8c23fd92b589fb60047f7494ce2ee9a6751d374f
jeri <jerimonski@gmail.com>
2025-09-16 18:38:59 -0300

    chore: fix import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react"
 import db from "@astrojs/db"
+import vercel from "@astrojs/vercel"
 
 export default defineConfig({
   output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
